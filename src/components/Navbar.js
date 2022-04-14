229cfd6176fb42b246fcc9d07f8ab315703ea87a
diskit <diskit.angmo@catalyst.sh>
2022-04-14 16:48:42 +0530

    navbar using sass

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { Link } from "gatsby"
+import React from "react"
+import "../styles/navbar.css"
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link className="home" to="/">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className="home" to="/about">
+            About
+          </Link>
+        </li>
+        <li>
+          <Link className="home" to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
